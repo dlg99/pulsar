8353d2bbbae5fa9df93c459bb6fb4871cb81333b
Andrey Yegorov <andrey.yegorov@datastax.com>
2024-09-13 12:24:07 -0700

    fail tests at the end

--- build/run_integration_group.sh
+++ build/run_integration_group.sh
@@ -118,8 +118,8 @@ test_group_sql() {
 }
 
 test_group_pulsar_io() {
-  mvn_run_integration_test "$@" -DintegrationTestSuiteFile=pulsar-io-sources.xml -DintegrationTests -Dgroups=source
-  mvn_run_integration_test "$@" -DintegrationTestSuiteFile=pulsar-io-sinks.xml -DintegrationTests -Dgroups=sink
+  mvn_run_integration_test "$@" -DintegrationTestSuiteFile=pulsar-io-sources.xml -fae -DintegrationTests -Dgroups=source
+  mvn_run_integration_test "$@" -DintegrationTestSuiteFile=pulsar-io-sinks.xml -fae -DintegrationTests -Dgroups=sink
 }
 
 test_group_pulsar_io_ora() {
