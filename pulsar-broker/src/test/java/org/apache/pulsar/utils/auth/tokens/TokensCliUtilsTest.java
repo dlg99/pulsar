cdb733e96f288b998f1f7bbfd2826f2cd2f4b46b
Andrey Yegorov <andrey.yegorov@datastax.com>
2025-06-18 10:29:14 -0700

    trim spaces

--- pulsar-broker/src/test/java/org/apache/pulsar/utils/auth/tokens/TokensCliUtilsTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/utils/auth/tokens/TokensCliUtilsTest.java
@@ -75,7 +75,7 @@ public class TokensCliUtilsTest {
             };
 
             new TokensCliUtils().execute(command);
-            String token = baoStream.toString();
+            String token = baoStream.toString().trim();
 
             Jwt<?, ?> jwt = Jwts.parser()
                     .setSigningKey(Decoders.BASE64.decode(secretKey))
@@ -109,7 +109,7 @@ public class TokensCliUtilsTest {
             };
 
             new TokensCliUtils().execute(command);
-            String token = baoStream.toString();
+            String token = baoStream.toString().trim();
 
             Instant start = (new Date().toInstant().plus(expireAsSec - 5, ChronoUnit.SECONDS));
             Instant stop = (new Date().toInstant().plus(expireAsSec + 5, ChronoUnit.SECONDS));
